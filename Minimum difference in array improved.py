36ee92efdca3827239d53763ddf252d139e3309c
Aidan <44209827+wittycodername@users.noreply.github.com>
2019-05-08 10:46:41 +0100

    Create Minimum difference in array improved.py

---
+++ Minimum difference in array improved.py
@@ -0,0 +1,30 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    arr = sorted(arr)
+    diffs = []
+    n = len(arr)
+    for i in range (1,n):
+                value = abs(arr[i]-arr[i-1])
+                diffs.append(value)
+    return (min(diffs))
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
